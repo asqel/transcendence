2c26b61a176facb32fa86fe09f954c38c8ddf1c3
asqel <axel.lleres@gmail.com>
2026-08-04 01:49:32 +0200

    fixed django to db to django to db to django to db ...

--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +87,7 @@ DATABASES = {
 		"NAME": os.getenv("MARIADB_DATABASE"),
 		"USER": os.getenv("MARIADB_USER"),
 		"PASSWORD": os.getenv("MARIADB_PASSWORD"),
-		"HOST": os.getenv("db"),
+		"HOST": "db",
 		"PORT": "3306",
 	}
 }
