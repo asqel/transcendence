b6ff0ca12397dc330a6406bbc206d802dfaa7f2a
asqel <axel.lleres@gmail.com>
2026-08-04 14:42:27 +0200

    on peux creer un compte waw

--- src/backend/test1/views.py
+++ src/backend/test1/views.py
@@ -1,9 +1,30 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+@csrf_exempt # sinon les cookies crients (aled)
 def test(request):
-	print("OH WAW UNE REQUET CEST DINGUE VRAIMENT")
-	print(dir(request))
+	try:
+		data = json.loads(request.body)
+		name = data.get("name", None)
+		mail = data.get("mail", None)
+		passwd = data.get("passwd", None)
+		if name and mail and passwd:
+			User.objects.create_user(name, mail, passwd)
+		if name and passwd:
+			user = authenticate(username=name, password=passwd)
+			return JsonResponse({
+				"message": str(user)
+			})
+		return JsonResponse({
+			"message": str(data)
+		})
 
-	return JsonResponse({
-		"message": "CA MARCHE ET CEST UN JSON"
-	})
+	except Exception as e:
+		return JsonResponse({
+
+			"message": str(e)
+		})
