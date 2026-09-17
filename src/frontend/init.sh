6a586a5f019bd363ec9213438dd92ff4171c7a59
Niedwin <G.Viure@outlook.fr>
2026-09-17 19:43:31 +0200

    resolve conflict
    resolve conflict

--- src/frontend/init.sh
+++ src/frontend/init.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -f "secrets/server.key" ] || [ ! -f "secrets/server.crt" ] ; then
-	echo "Error: Missing frontend server certificat or key" >&2 
+	echo "Error: Missing frontend server certificat or key" >&2
 	exit 1
 fi
 
@@ -10,18 +10,11 @@ fi
 mkdir -p /etc/nginx/ssl/
 cp secrets/* /etc/nginx/ssl/
 
-<<<<<<< Updated upstream
 # copy conf + site
-=======
->>>>>>> Stashed changes
 cp nginx/nginx.conf /etc/nginx/
 rm -rf /var/www/html/
 mkdir -p /var/www/
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 apk add --no-cache nodejs npm
 npm --prefix ./site install --legacy-peer-deps
 npm --prefix ./site run build
